7f166aa086e6dd72ccf9ba5a16edc93fce3549fb
AritroSinha <sinhaaritro@gmail.com>
2021-10-24 04:21:38 +0530

    Removed path, tags from user. Added theme.

--- models/user.js
+++ models/user.js
@@ -2,45 +2,48 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-const defaultPath = JSON.stringify({
-    root: {},
-});
-
-const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        require: [true, `Name is required`],
-        maxlength: 50,
-        minLength: 3,
+const UserSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            require: [true, `Name is required`],
+            maxlength: 50,
+            minLength: 3,
+        },
+        email: {
+            type: String,
+            require: [true, `Email is required`],
+            match: [
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                "Please provide a valid email",
+            ],
+            unique: true,
+        },
+        password: {
+            type: String,
+            require: [true, `Password is required`],
+            minLength: 5,
+        },
+        theme: {
+            type: String,
+            default: "dark",
+            enum: {
+                values: ["light", "dark"],
+                message: "Theme is not supported",
+            },
+        },
     },
-    email: {
-        type: String,
-        require: [true, `Email is required`],
-        match: [
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-            "Please provide a valid email",
-        ],
-        unique: true,
-    },
-    password: {
-        type: String,
-        require: [true, `Password is required`],
-        minLength: 5,
-    },
-    tags: {
-        type: [String],
-        default: ["delete", "archive"],
-    },
-    path: {
-        type: String,
-        default: defaultPath,
-    },
-});
+    { timestamps: true }
+);
 
 UserSchema.pre("save", async function () {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
 });
+
+UserSchema.methods.isPasswordCorrect = async function (givenPassword) {
+    return await bcrypt.compare(givenPassword, this.password);
+};
 
 UserSchema.methods.createJWT = function () {
     return jwt.sign(
@@ -48,10 +51,6 @@ UserSchema.methods.createJWT = function () {
         process.env.JWT_SECRET,
         { expiresIn: process.env.JWT_LIFETIME }
     );
-};
-
-UserSchema.methods.isPasswordCorrect = async function (givenPassword) {
-    return await bcrypt.compare(givenPassword, this.password);
 };
 
 module.exports = mongoose.model("User", UserSchema);
