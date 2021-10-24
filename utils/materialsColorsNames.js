1bf5602da71277fa9c09afa4bdadf6da0eda4a7f
AritroSinha <sinhaaritro@gmail.com>
2021-10-24 14:39:40 +0530

    added material color name

---
+++ utils/materialsColorsNames.js
@@ -0,0 +1,22 @@
+const materialColorsNames = [
+    "red",
+    "pink",
+    "deeppurple",
+    "indigo",
+    "blue",
+    "lightblue",
+    "cyan",
+    "teal",
+    "green",
+    "lightgreen",
+    "lime",
+    "yellow",
+    "amber",
+    "orange",
+    "deeporange",
+    "brown",
+    "grey",
+    "bluegrey",
+];
+
+module.exports = materialColorsNames;
