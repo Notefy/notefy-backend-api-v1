c79e71caa401b30bf81f71565b6cc183a34b396d
AritroSinha <sinhaaritro@gmail.com>
2021-10-25 11:55:35 +0530

    added user to req object

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -24,6 +24,7 @@ const auth = async (req, res, next) => {
                 .json({ msg: "Invalid Credentials" });
 
         req.userID = payload.userID;
+        req.user = user;
         next();
     } catch (error) {
         return res
